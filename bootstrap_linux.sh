23ed2f0cf8e4abd06d9846dc7f52581011150fa3
Alexey Kuhta <electricsix23@gmail.com>
2025-01-30 01:41:21 +0100

    some fix

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-conan install . --output-folder=cmake-build-debug --build=missing --profile:h=debugprofile_linux.txt --profile:b=debugprofile_linux.txt
+conan install . --output-folder=cmake-build-debug --build=missing --profile:h=debugprofile_linux.txt --profile:b=debugprofile_linux.txt -c tools.system.package_manager:mode=install
 cd cmake-build-debug
 cmake .. --preset=conan-debug
