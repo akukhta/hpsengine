ce605a3dd599bfdcd8768608b8835577d8b52067
Alexey Kuhta <electricsix23@gmail.com>
2025-01-30 01:38:28 +0100

    sudo conan removed

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo conan install . --output-folder=cmake-build-debug --build=missing --profile:h=debugprofile_linux.txt --profile:b=debugprofile_linux.txt
+conan install . --output-folder=cmake-build-debug --build=missing --profile:h=debugprofile_linux.txt --profile:b=debugprofile_linux.txt
 cd cmake-build-debug
 cmake .. --preset=conan-debug
